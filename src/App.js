92751f055c0df2ccb3a675cdee7fb9f9f51dda40
Gabriel Town <rieltown@gmail.com>
2023-03-28 11:17:06 -0600

    added setState

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ constructor(){
   super();
 
   this.state = {
-    name: 'Gabe'
+    name: {firstName: 'Gabe', lastName: 'Town'},
+    company: 'Town of Gabriel'
   }
 }
 
@@ -17,8 +18,8 @@ render(){
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi {this.state.name}</p>
-        <button>change name</button>
+        <p>Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}</p>
+        <button onClick={() => {this.setState({name: {firstName: 'Timmy', lastName: 'Time'}})}}>change name</button>
       </header>
     </div>
   );
