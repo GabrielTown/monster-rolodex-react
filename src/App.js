b8e117046a7686f7bc3c9e61429fb5bc82455ec3
Gabriel Town <rieltown@gmail.com>
2023-03-28 11:32:21 -0600

    used setState as function with callback

--- src/App.js
+++ src/App.js
@@ -19,7 +19,22 @@ render(){
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}</p>
-        <button onClick={() => {this.setState({name: {firstName: 'Timmy', lastName: 'Time'}})}}>change name</button>
+        {/* <button onClick={() => {this.setState({name: {firstName: 'Timmy', lastN{name: {firstName: 'Timmy', lastName: 'Time'}})}}>change name</button> */}
+        
+        <button onClick={() => {
+          this.setState(
+            () => {
+              return {
+                name: {firstName: 'Timmy', lastName: 'Time'},
+              }
+            }, 
+            // callback function
+            () => {
+              console.log(this.state)
+            }
+          );
+        }}
+        >change name</button>
       </header>
     </div>
   );
