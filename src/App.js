491af554653670f7c16049d289c87a2c47b230e8
Gabriel Town <rieltown@gmail.com>
2023-03-29 11:16:12 -0600

    added componentDidMount (fetch data)

--- src/App.js
+++ src/App.js
@@ -1,42 +1,41 @@
-import { Component } from 'react';
-import './App.css';
+import { Component } from 'react'
+import './App.css'
 
 class App extends Component {
 
 constructor(){
-  super();
+  super()
 
   this.state = {
-   monsters: [
-    {
-      name: 'Linda',
-      id: '23sdafar234',
-     },
-     {
-      name: "Frank",
-      id: '23sdafa4',
+   monsters: [],
+  }
+  console.log('constructor')
+}
 
-     },
-     {
-      name: "Jacky",
-      id: '23afar234',
-     },
-     {
-      name: "James",
-      id: '23sdafar234asdf',
-     },
-   ],
-  };
+componentDidMount(){
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => this.setState( 
+      () => {
+        return {monsters: users}
+      },
+      () => {
+        console.log(this.state)
+      }
+    ))
+    console.log('componentDidMount')
 }
 
 render(){
+  console.log('render')
+
   return (
     <div className="App">
       {this.state.monsters.map((monster) => {
-        return <h1 key={monster.id}>{monster.name}</h1>;
+        return <h1 key={monster.id}>{monster.name}</h1>
       })}
     </div>
-  );
+  )
 }
 
 }
